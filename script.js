183941d2058f73d3b41ea5974a1b8a0717f3eca7
sumitjamdhade <sumitvjamdhade@gmail.com>
2025-06-23 00:00:56 +0530

    songs in the library can be played now

--- script.js
+++ script.js
@@ -6,25 +6,44 @@ let songs = [
     "http://127.0.0.1:5500/Songs/Ricky%20Montgomery%20%20Line%20Without%20a%20Hook%20Official%20Music%20Video.mp3"
 
 
-    //i may fetch songs using these urls by applying a for loop 
+    //i may fetch songs using these urls, by applying a for loop 
 ]
+
+let currentaudio = new Audio()
+
+const playAudio = (track)=>{
+
+  currentaudio.src = "/Songs/" + track
+  currentaudio.play()
+}
 
 async function getSongs() {
 
-    var audio = new Audio(songs[0]);
+    // var audio = new Audio(songs[0]);
     // audio.play()
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li> ${song.split("Songs/")[1].replaceAll("%20", " ")} </li>`;
-        
-        
+    for (const eachsong of songs) {
+        let song = eachsong.split("Songs/")[1].replaceAll("%20", " ")
+       
+        songUL.innerHTML = songUL.innerHTML + `<li class="flex align-center justify-between">
+                            <img class="invert"  src="music.svg" alt="music" srcset="">
+                            <div class="info">  <div> ${song} </div>
+                            </div>
+                          <div class="playnow flex align-center ">
+                                <img class="invert" src="play.svg" alt="" srcset="">
+                                Play now
+                              </div>
+                            </li>`;
     }
 
-
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(element => {
+      element.addEventListener("click", e=>{
+            console.log(element.querySelector(".info").firstElementChild.innerHTML)
+            playAudio(element.querySelector(".info").firstElementChild.innerHTML.trim())
+      })
+      
+    });
 }
 
 getSongs();
-
-
-
